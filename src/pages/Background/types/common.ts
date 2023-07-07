cb7f324e8b1f4a7d517677cb547b3451d4c2c31b
ananya150 <akblockchain15@gmail.com>
2023-07-07 19:59:38 +0530

    feat: added basic types

---
+++ src/pages/Background/types/common.ts
@@ -0,0 +1,16 @@
+/**
+ * Named type for strings that should be hexadecimal numbers.
+ */
+export type HexString = string;
+
+/**
+ * Named type for strings that should be domain names.
+ */
+export type DomainName = string;
+
+/**
+ * Named type for strings that should be URIs.
+ */
+export type URI = string;
+
+export type SCWImplementation = string;
