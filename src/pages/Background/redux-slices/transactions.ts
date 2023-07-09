2efef1fb8e1a42c7b1bb4244b4436984881ba970
ananya150 <akblockchain15@gmail.com>
2023-07-09 19:08:37 +0530

    feat: added remaining app (Errors)

--- src/pages/Background/redux-slices/transactions.ts
+++ src/pages/Background/redux-slices/transactions.ts
@@ -1,234 +1,231 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type {  BigNumberish, BytesLike } from "ethers";
-import { AccessListish } from 'ethers/lib/utils.js';
-import { createBackgroundAsyncThunk } from "./utils";
-import { RootState } from ".";
-import ProviderBridgeService from "../services/provider-bridge";
-import KeyringService from "../services/keyring";
-
-export type EthersTransactionRequest = {
-    to: string;
-    from?: string;
-    nonce?: BigNumberish;
-  
-    gasLimit?: BigNumberish;
-    gasPrice?: BigNumberish;
-  
-    data?: BytesLike;
-    value?: BigNumberish;
-    chainId?: number;
-  
-    type?: number;
-    accessList?: AccessListish;
-  
-    maxPriorityFeePerGas?: BigNumberish;
-    maxFeePerGas?: BigNumberish;
-  
-    customData?: Record<string, any>;
-  };
-export type PromiseOrValue<T> = T | Promise<T>;
-export type UserOperationStruct = {
-    sender: PromiseOrValue<string>;
-    nonce: PromiseOrValue<BigNumberish>;
-    initCode: PromiseOrValue<BytesLike>;
-    callData: PromiseOrValue<BytesLike>;
-    callGasLimit: PromiseOrValue<BigNumberish>;
-    verificationGasLimit: PromiseOrValue<BigNumberish>;
-    preVerificationGas: PromiseOrValue<BigNumberish>;
-    maxFeePerGas: PromiseOrValue<BigNumberish>;
-    maxPriorityFeePerGas: PromiseOrValue<BigNumberish>;
-    paymasterAndData: PromiseOrValue<BytesLike>;
-    signature: PromiseOrValue<BytesLike>;
-};
+import { UserOperationStruct } from '@account-abstraction/contracts';
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '.';
+import KeyringService from '../services/keyring';
+import ProviderBridgeService from '../services/provider-bridge';
+import { EthersTransactionRequest } from '../services/types';
+import { createBackgroundAsyncThunk } from './utils';
 
 export type TransactionState = {
-    transactionRequest?: EthersTransactionRequest;
-    transactionsRequest?: EthersTransactionRequest[];
-    modifiedTransactionRequest?: EthersTransactionRequest;
-  
-    requestOrigin?: string;
-    userOperationRequest?: Partial<UserOperationStruct>;
-    unsignedUserOperation?: UserOperationStruct;
-  };
+  transactionRequest?: EthersTransactionRequest;
+  transactionsRequest?: EthersTransactionRequest[];
+  modifiedTransactionRequest?: EthersTransactionRequest;
+
+  requestOrigin?: string;
+  userOperationRequest?: Partial<UserOperationStruct>;
+  unsignedUserOperation?: UserOperationStruct;
+};
 
 export const initialState: TransactionState = {
-    transactionsRequest: undefined,
-    transactionRequest: undefined,
-    userOperationRequest: undefined,
-    unsignedUserOperation: undefined,
-  };
-  
+  transactionsRequest: undefined,
+  transactionRequest: undefined,
+  userOperationRequest: undefined,
+  unsignedUserOperation: undefined,
+};
 
 type SigningReducers = {
+  sendTransactionRequest: (
+    state: TransactionState,
+    {
+      payload,
+    }: {
+      payload: {
+        transactionRequest: EthersTransactionRequest;
+        origin: string;
+      };
+    }
+  ) => TransactionState;
+  sendTransactionsRequest: (
+    state: TransactionState,
+    {
+      payload,
+    }: {
+      payload: {
+        transactionsRequest: EthersTransactionRequest[];
+        origin: string;
+      };
+    }
+  ) => TransactionState;
+  setModifyTransactionRequest: (
+    state: TransactionState,
+    {
+      payload,
+    }: {
+      payload: EthersTransactionRequest;
+    }
+  ) => TransactionState;
+  sendUserOperationRquest: (
+    state: TransactionState,
+    { payload }: { payload: UserOperationStruct }
+  ) => TransactionState;
+  setUnsignedUserOperation: (
+    state: TransactionState,
+    { payload }: { payload: UserOperationStruct }
+  ) => TransactionState;
+  clearTransactionState: (state: TransactionState) => TransactionState;
+};
+
+const transactionsSlice = createSlice<
+  TransactionState,
+  SigningReducers,
+  'signing'
+>({
+  name: 'signing',
+  initialState,
+  reducers: {
     sendTransactionRequest: (
-      state: TransactionState,
+      state,
       {
-        payload,
+        payload: { transactionRequest, origin },
       }: {
         payload: {
           transactionRequest: EthersTransactionRequest;
           origin: string;
         };
       }
-    ) => TransactionState;
+    ) => {
+      return {
+        ...state,
+        transactionRequest: transactionRequest,
+        requestOrigin: origin,
+      };
+    },
     sendTransactionsRequest: (
-      state: TransactionState,
+      state,
       {
-        payload,
+        payload: { transactionsRequest, origin },
       }: {
         payload: {
           transactionsRequest: EthersTransactionRequest[];
           origin: string;
         };
       }
-    ) => TransactionState;
+    ) => {
+      return {
+        ...state,
+        transactionsRequest: transactionsRequest,
+        requestOrigin: origin,
+      };
+    },
     setModifyTransactionRequest: (
-      state: TransactionState,
+      state,
       {
         payload,
       }: {
         payload: EthersTransactionRequest;
       }
-    ) => TransactionState;
+    ) => ({
+      ...state,
+      modifiedTransactionRequest: payload,
+    }),
     sendUserOperationRquest: (
-      state: TransactionState,
+      state,
       { payload }: { payload: UserOperationStruct }
-    ) => TransactionState;
+    ) => ({
+      ...state,
+      userOperationRequest: payload,
+    }),
     setUnsignedUserOperation: (
-      state: TransactionState,
+      state,
       { payload }: { payload: UserOperationStruct }
-    ) => TransactionState;
-    clearTransactionState: (state: TransactionState) => TransactionState;
-  };
-  
-const transactionsSlice = createSlice<
-    TransactionState,
-    SigningReducers,
-    'signing'
-  >({
-    name: 'signing',
-    initialState,
-    reducers: {
-      sendTransactionRequest: (
-        state,
-        {
-          payload: { transactionRequest, origin },
-        }: {
-          payload: {
-            transactionRequest: EthersTransactionRequest;
-            origin: string;
-          };
-        }
-      ) => {
-        return {
-          ...state,
-          transactionRequest: transactionRequest,
-          requestOrigin: origin,
-        };
-      },
-      sendTransactionsRequest: (
-        state,
-        {
-          payload: { transactionsRequest, origin },
-        }: {
-          payload: {
-            transactionsRequest: EthersTransactionRequest[];
-            origin: string;
-          };
-        }
-      ) => {
-        return {
-          ...state,
-          transactionsRequest: transactionsRequest,
-          requestOrigin: origin,
-        };
-      },
-      setModifyTransactionRequest: (
-        state,
-        {
-          payload,
-        }: {
-          payload: EthersTransactionRequest;
-        }
-      ) => ({
-        ...state,
-        modifiedTransactionRequest: payload,
-      }),
-      sendUserOperationRquest: (
-        state,
-        { payload }: { payload: UserOperationStruct }
-      ) => ({
-        ...state,
-        userOperationRequest: payload,
-      }),
-      setUnsignedUserOperation: (
-        state,
-        { payload }: { payload: UserOperationStruct }
-      ) => ({
-        ...state,
-        unsignedUserOperation: payload,
-      }),
-      clearTransactionState: (state) => ({
-        ...state,
-        typedDataRequest: undefined,
-        signDataRequest: undefined,
-        transactionRequest: undefined,
-        transactionsRequest: undefined,
-        modifiedTransactionRequest: undefined,
-        requestOrigin: undefined,
-        userOperationRequest: undefined,
-        unsignedUserOperation: undefined,
-      }),
-    },
-  });
-  
+    ) => ({
+      ...state,
+      unsignedUserOperation: payload,
+    }),
+    clearTransactionState: (state) => ({
+      ...state,
+      typedDataRequest: undefined,
+      signDataRequest: undefined,
+      transactionRequest: undefined,
+      transactionsRequest: undefined,
+      modifiedTransactionRequest: undefined,
+      requestOrigin: undefined,
+      userOperationRequest: undefined,
+      unsignedUserOperation: undefined,
+    }),
+  },
+});
+
 export const {
-    sendTransactionRequest,
-    sendTransactionsRequest,
-    setModifyTransactionRequest,
-    sendUserOperationRquest,
-    setUnsignedUserOperation,
-    clearTransactionState,
-  } = transactionsSlice.actions;
-  
+  sendTransactionRequest,
+  sendTransactionsRequest,
+  setModifyTransactionRequest,
+  sendUserOperationRquest,
+  setUnsignedUserOperation,
+  clearTransactionState,
+} = transactionsSlice.actions;
+
 export default transactionsSlice.reducer;
 
-export const createUnsignedUserOp = createBackgroundAsyncThunk(
-    'transactions/createUnsignedUserOp',
-    async (
-      { address, context }: { address: string; context?: any },
-      { dispatch, extra: { mainServiceManager } }
-    ) => {
-      const keyringService = mainServiceManager.getService(
-        KeyringService.name
-      ) as KeyringService;
-  
-      const state = mainServiceManager.store.getState() as RootState;
-      const transactionRequest = state.transactions.transactionRequest;
-  
-      if (transactionRequest) {
-        const userOp = await keyringService.createUnsignedUserOp(
-          address,
-          transactionRequest,
-          context
-        );
-        dispatch(setUnsignedUserOperation(userOp));
-      }
-    }
-  );
-  
-export const rejectTransaction = createBackgroundAsyncThunk(
-    'transactions/rejectTransaction',
-    async (address: string, { dispatch, extra: { mainServiceManager } }) => {
+export const sendTransaction = createBackgroundAsyncThunk(
+  'transactions/sendTransaction',
+  async (
+    { address, context }: { address: string; context?: any },
+    { dispatch, extra: { mainServiceManager } }
+  ) => {
+    const keyringService = mainServiceManager.getService(
+      KeyringService.name
+    ) as KeyringService;
+
+    const state = mainServiceManager.store.getState() as RootState;
+    const unsignedUserOp = state.transactions.unsignedUserOperation;
+    const origin = state.transactions.requestOrigin;
+
+    if (unsignedUserOp) {
+      const signedUserOp = await keyringService.signUserOpWithContext(
+        address,
+        unsignedUserOp,
+        context
+      );
+      const txnHash = keyringService.sendUserOp(address, signedUserOp);
+
       dispatch(clearTransactionState());
-  
-      const requestOrigin = (mainServiceManager.store.getState() as RootState)
-        .transactions.requestOrigin;
-  
+
       const providerBridgeService = mainServiceManager.getService(
         ProviderBridgeService.name
       ) as ProviderBridgeService;
-  
-      providerBridgeService.rejectRequest(requestOrigin || '', '');
+
+      providerBridgeService.resolveRequest(origin || '', txnHash);
     }
-  );
+  }
+);
+
+export const createUnsignedUserOp = createBackgroundAsyncThunk(
+  'transactions/createUnsignedUserOp',
+  async (
+    { address, context }: { address: string; context?: any },
+    { dispatch, extra: { mainServiceManager } }
+  ) => {
+    const keyringService = mainServiceManager.getService(
+      KeyringService.name
+    ) as KeyringService;
+
+    const state = mainServiceManager.store.getState() as RootState;
+    const transactionRequest = state.transactions.transactionRequest;
+
+    if (transactionRequest) {
+      const userOp = await keyringService.createUnsignedUserOp(
+        address,
+        transactionRequest,
+        context
+      );
+      dispatch(setUnsignedUserOperation(userOp));
+    }
+  }
+);
+
+export const rejectTransaction = createBackgroundAsyncThunk(
+  'transactions/rejectTransaction',
+  async (address: string, { dispatch, extra: { mainServiceManager } }) => {
+    dispatch(clearTransactionState());
+
+    const requestOrigin = (mainServiceManager.store.getState() as RootState)
+      .transactions.requestOrigin;
+
+    const providerBridgeService = mainServiceManager.getService(
+      ProviderBridgeService.name
+    ) as ProviderBridgeService;
+
+    providerBridgeService.rejectRequest(requestOrigin || '', '');
+  }
+);
---
+++ src/pages/Popup/pages/sign-transaction-request/sign-transaction-request.tsx
@@ -0,0 +1,185 @@
+import { UserOperationStruct } from '@account-abstraction/contracts';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { BigNumber, ethers } from 'ethers';
+import React, { useCallback, useState } from 'react';
+import {
+  AccountImplementations,
+  ActiveAccountImplementation,
+} from '../../../App/constants';
+import {
+  useBackgroundDispatch,
+  useBackgroundSelector,
+} from '../../../App/hooks';
+import {
+  getAccountInfo,
+  getActiveAccount,
+} from '../../../Background/redux-slices/selectors/accountSelectors';
+import { selectCurrentOriginPermission } from '../../../Background/redux-slices/selectors/dappPermissionSelectors';
+import { getActiveNetwork } from '../../../Background/redux-slices/selectors/networkSelectors';
+import {
+  selectCurrentPendingModifiedSendTransactionRequest,
+  selectCurrentPendingSendTransactionRequest,
+  selectCurrentPendingSendTransactionUserOp,
+} from '../../../Background/redux-slices/selectors/transactionsSelectors';
+import {
+  createUnsignedUserOp,
+  rejectTransaction,
+  sendTransaction,
+} from '../../../Background/redux-slices/transactions';
+import { EthersTransactionRequest } from '../../../Background/services/types';
+import { setModifyTransactionRequest } from '../../../Background/redux-slices/transactions';
+
+const SignTransactionComponent =
+  AccountImplementations[ActiveAccountImplementation].Transaction;
+
+const SignTransactionRequest: any = () => {
+  const [stage, setStage] = useState<{
+    stage:
+      | 'pre-transaction-confirmation'
+      | 'transaction-confirmation'
+      | 'post-transaction-confirmation';
+    context?: any;
+  }>({
+    stage: 'pre-transaction-confirmation',
+  });
+
+  const backgroundDispatch = useBackgroundDispatch();
+  const activeAccount = useBackgroundSelector(getActiveAccount);
+
+  const sendTransactionRequest = useBackgroundSelector(
+    selectCurrentPendingSendTransactionRequest
+  );
+
+  const sendModiefiedTransactionRequest = useBackgroundSelector(
+    selectCurrentPendingModifiedSendTransactionRequest
+  );
+
+  const pendingUserOp = useBackgroundSelector(
+    selectCurrentPendingSendTransactionUserOp
+  );
+
+  const onSend = useCallback(
+    async (context?: any) => {
+      if (activeAccount)
+        await backgroundDispatch(
+          sendTransaction({
+            address: activeAccount,
+            context,
+          })
+        );
+      window.close();
+    },
+    [activeAccount, backgroundDispatch]
+  );
+
+  const onCompletePreTransactionConfirmation = useCallback(
+    async (modifiedTransaction?: EthersTransactionRequest, context?: any) => {
+      if (!activeAccount) return;
+      backgroundDispatch(
+        createUnsignedUserOp({ address: activeAccount, context })
+      );
+      backgroundDispatch(
+        setModifyTransactionRequest(
+          modifiedTransaction || sendTransactionRequest.transactionRequest!
+        )
+      );
+      setStage({
+        stage: 'transaction-confirmation',
+        context,
+      });
+    },
+    [
+      activeAccount,
+      backgroundDispatch,
+      sendTransactionRequest.transactionRequest,
+    ]
+  );
+
+  const onCompleteTransactionConfirmation = useCallback(
+    async (context?: any) => {
+      setStage({
+        stage: 'post-transaction-confirmation',
+        context,
+      });
+    },
+    []
+  );
+
+  const onCompletePostTransactionConfirmation = useCallback(
+    async (context?: any) => {
+      if (activeAccount) {
+        onSend(context);
+      }
+    },
+    [activeAccount, onSend]
+  );
+
+  const onReject = useCallback(async () => {
+    if (activeAccount)
+      await backgroundDispatch(rejectTransaction(activeAccount));
+    window.close();
+  }, [backgroundDispatch, activeAccount]);
+
+  switch (stage.stage) {
+    case 'pre-transaction-confirmation':
+      return SignTransactionComponent?.PreTransactionConfirmation ? (
+        <SignTransactionComponent.PreTransactionConfirmation
+          onReject={onReject}
+          transaction={sendTransactionRequest.transactionRequest!}
+          onComplete={onCompletePreTransactionConfirmation}
+        />
+      ) : (
+        onCompletePreTransactionConfirmation(
+          sendTransactionRequest.transactionRequest,
+          {}
+        )
+      );
+    case 'transaction-confirmation':
+      return SignTransactionComponent?.TransactionConfirmation &&
+        sendModiefiedTransactionRequest.transactionRequest &&
+        pendingUserOp ? (
+        <SignTransactionComponent.TransactionConfirmation
+          context={stage.context}
+          onReject={onReject}
+          userOp={pendingUserOp}
+          transaction={sendModiefiedTransactionRequest.transactionRequest}
+          onComplete={onCompleteTransactionConfirmation}
+        />
+      ) : (
+        <Container
+          sx={{
+            height: '100vh',
+            justifyContent: 'center',
+            alignItems: 'center',
+            display: 'flex',
+          }}
+        >
+          <CircularProgress />
+        </Container>
+      );
+    case 'post-transaction-confirmation':
+      return (
+        SignTransactionComponent?.PostTransactionConfirmation &&
+        pendingUserOp && (
+          <SignTransactionComponent.PostTransactionConfirmation
+            context={stage.context}
+            onReject={onReject}
+            userOp={pendingUserOp}
+            transaction={sendModiefiedTransactionRequest.transactionRequest!}
+            onComplete={onCompletePostTransactionConfirmation}
+          />
+        )
+      );
+  }
+};
+
+export default SignTransactionRequest;
