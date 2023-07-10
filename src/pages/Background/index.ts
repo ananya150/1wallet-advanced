5c3dcb87717d6de25f8d7539aaf9c74f5e43c3bb
ananya150 <akblockchain15@gmail.com>
2023-07-10 08:01:24 +0530

    feat: messaging demo between popup and background

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,1 +1,14 @@
-console.log("Starting main service in background script but changed")
+/// <reference types="chrome"/>
+import { Vault } from "./vault";
+console.log("Statting main service");
+
+const vault = new Vault();
+
+chrome.runtime.onMessage.addListener(
+    function(request: any, sender: any, sendResponse: any){
+        console.log("request is ", request);
+        console.log("Sender is ", sender);
+        sendResponse({message: "Received"});
+        return true;
+    }
+)
--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 const Popup = () => {
+  const [received, setReceived] = useState<any>(null)
+  const onSend = async () => {
+    chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+      console.log("response is ",response);
+      setReceived(response.message)
+    });
+  }
+
   return (
-    <div className="container">Popup</div>
+    <div className="container">
+      <button onClick={onSend}>SendMEssage</button>
+      <div>Received Message is : {received}</div>
+    </div>
   )
 }
 
