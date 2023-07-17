eb28a8c6ed6ce2455d8745c419c6d79509705021
ananya150 <akblockchain15@gmail.com>
2023-07-17 17:46:44 +0530

    feat: added tx failed interface

--- src/pages/Popup/components/ConfirmTokenTransfer.tsx
+++ src/pages/Popup/components/ConfirmTokenTransfer.tsx
@@ -10,7 +10,8 @@ import Divider from '@mui/material/Divider';
 import PaymasterSelect from './ui/Select';
 import { getWalletInfo } from '../../utils';
 import { buildExecuteUserOp, getCallData, sendExecuteUserOp } from '../../userOp';
-
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
 
 type pageProps = {
     token: Token;
@@ -20,12 +21,25 @@ type pageProps = {
     setConfirmation: any;
 }
 
+type TransactionState = {
+    isLoading: boolean;
+    isSuccess: boolean;
+    isFailure: boolean;
+}
+
+const initialTransactionState: TransactionState = {
+    isLoading: false,
+    isSuccess: false,
+    isFailure: true
+}
+
 const ConfirmTokenTransfer = ({setConfirmation, address , setValue, amount , token}: pageProps) => {
 
     function delay(ms: number) {
         return new Promise(resolve => setTimeout(resolve, ms));
       }    
-    const [copied, setCopied] = useState(false)
+    const [copied, setCopied] = useState(false);
+    const [transactionState, setTransactionState] = useState<TransactionState>(initialTransactionState);
     const handleClose = () => {
         setValue('tokens')
     }
@@ -67,68 +81,132 @@ const ConfirmTokenTransfer = ({setConfirmation, address , setValue, amount , tok
     }
 
     return (
-        <div style={{height:'450px', width:'365px', display:'flex', justifyContent:'space-between', flexDirection:'column', overflowX:'hidden' ,overflowY:'scroll'}}>
-            <div>
-                <div style={{display:'flex', justifyContent:'space-between', height:'30px', marginTop:'10px'}}>
-                    <IconButton onClick={() => {setConfirmation(false)}}>
-                        <ArrowBackIcon sx={{color:'white', marginLeft:'6px'}} />
-                    </IconButton>
-                    <div className='headingHome' style={{marginRight:'20px', fontSize:'24px', fontWeight:'600', color:'#D3D3D3'}}>
-                        Confirm Send
-                    </div>
-                    <div></div>
-                </div>
-                <div className='amount' style={{display: 'flex', justifyContent: 'center', marginTop: '30px', fontSize:'35px', fontWeight:'600',color:'#fefdf9',width:'365px'}}>
-                    {amount} {token.contract_ticker_symbol}
-                </div>
-                <div style={{display: 'flex', justifyContent: 'center', marginTop: '8px', fontSize:'15px', fontWeight:'400',color:'#808080',width:'365px'}}>
-                    to &nbsp; <a data-tooltip-id="address-tooltip" onClick={handleCopy} style={{cursor:'pointer'}}
-                          data-tooltip-content={copied ? 'Copied' : 'Copy to clipboard'}
-                          data-tooltip-place="top">{address.slice(0,5)}....{address.slice(-5)}</a>
-                </div>
-                <div style={{display:'flex', justifyContent:'center', marginTop:'40px', width:'365px'}}>
-                    <div style={{borderRadius:'10px',marginLeft:'25px', marginRight:'20px', background:'#2a2a2a'}}>
 
-                        <div style={{display:'flex', justifyContent:'space-between', paddingTop:'10px', paddingBottom:'10px', width:'300px'}}>
-                            <div style={{color:'#808080',  marginLeft:'15px', fontSize:'17px'}}>
-                                Network
+        <>
+        
+        {
+            !transactionState.isLoading && !transactionState.isFailure && !transactionState.isSuccess &&
+                <div style={{height:'450px', width:'365px', display:'flex', justifyContent:'space-between', flexDirection:'column', overflowX:'hidden' ,overflowY:'scroll'}}>
+                    <div>
+                        <div style={{display:'flex', justifyContent:'space-between', height:'30px', marginTop:'10px'}}>
+                            <IconButton onClick={() => {setConfirmation(false)}}>
+                                <ArrowBackIcon sx={{color:'white', marginLeft:'6px'}} />
+                            </IconButton>
+                            <div className='headingHome' style={{marginRight:'20px', fontSize:'24px', fontWeight:'600', color:'#D3D3D3'}}>
+                                Confirm Send
                             </div>
-                            <div style={{color:'#fefdf9', marginRight:'15px', fontSize:'17px'}}>
-                                Mumbai
-                            </div>
+                            <div></div>
                         </div>
-                        <Divider sx={{color:'black'}} />
-                        <div style={{display:'flex', justifyContent:'space-between', paddingTop:'10px', paddingBottom:'10px', width:'300px'}}>
-                            <div style={{color:'#808080',  marginLeft:'15px', fontSize:'17px'}}>
-                                Network Fee
-                            </div>
-                            <div style={{color:'#fefdf9', marginRight:'15px', fontSize:'17px'}}>
-                                $ 0.004
-                            </div>
+                        <div className='amount' style={{display: 'flex', justifyContent: 'center', marginTop: '30px', fontSize:'35px', fontWeight:'600',color:'#fefdf9',width:'365px'}}>
+                            {amount} {token.contract_ticker_symbol}
                         </div>
-                        <Divider sx={{color:'black'}} />
-                        <div style={{display:'flex', justifyContent:'space-between', paddingTop:'10px', paddingBottom:'10px', width:'300px'}}>
-                            <div style={{color:'#808080',  marginLeft:'15px', fontSize:'17px', display:'flex', flexDirection:'column', justifyContent:'center' }}>
-                                Fee
-                            </div>
-                            <div style={{color:'#fefdf9', marginRight:'15px', fontSize:'17px'}}>
-                                <PaymasterSelect selectedValue={selectedValue} setSelectedValue={setSelectedValue} handleSelectChange={handleSelectChange} />
+                        <div style={{display: 'flex', justifyContent: 'center', marginTop: '8px', fontSize:'15px', fontWeight:'400',color:'#808080',width:'365px'}}>
+                            to &nbsp; <a data-tooltip-id="address-tooltip" onClick={handleCopy} style={{cursor:'pointer'}}
+                                data-tooltip-content={copied ? 'Copied' : 'Copy to clipboard'}
+                                data-tooltip-place="top">{address.slice(0,5)}....{address.slice(-5)}</a>
+                        </div>
+                        <div style={{display:'flex', justifyContent:'center', marginTop:'40px', width:'365px'}}>
+                            <div style={{borderRadius:'10px',marginLeft:'25px', marginRight:'20px', background:'#2a2a2a'}}>
+
+                                <div style={{display:'flex', justifyContent:'space-between', paddingTop:'10px', paddingBottom:'10px', width:'300px'}}>
+                                    <div style={{color:'#808080',  marginLeft:'15px', fontSize:'17px'}}>
+                                        Network
+                                    </div>
+                                    <div style={{color:'#fefdf9', marginRight:'15px', fontSize:'17px'}}>
+                                        Mumbai
+                                    </div>
+                                </div>
+                                <Divider sx={{color:'black'}} />
+                                <div style={{display:'flex', justifyContent:'space-between', paddingTop:'10px', paddingBottom:'10px', width:'300px'}}>
+                                    <div style={{color:'#808080',  marginLeft:'15px', fontSize:'17px'}}>
+                                        Network Fee
+                                    </div>
+                                    <div style={{color:'#fefdf9', marginRight:'15px', fontSize:'17px'}}>
+                                        $ 0.004
+                                    </div>
+                                </div>
+                                <Divider sx={{color:'black'}} />
+                                <div style={{display:'flex', justifyContent:'space-between', paddingTop:'10px', paddingBottom:'10px', width:'300px'}}>
+                                    <div style={{color:'#808080',  marginLeft:'15px', fontSize:'17px', display:'flex', flexDirection:'column', justifyContent:'center' }}>
+                                        Fee
+                                    </div>
+                                    <div style={{color:'#fefdf9', marginRight:'15px', fontSize:'17px'}}>
+                                        <PaymasterSelect selectedValue={selectedValue} setSelectedValue={setSelectedValue} handleSelectChange={handleSelectChange} />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div style={{marginBottom:'20px', marginTop:'30px',  background:'#222222', width:'365px'}}>
+                        <div style={{marginLeft:'20px', marginRight:'15px', display:'flex', justifyContent:'space-between'}}>
+                            <BlackButton text='Close' onClick={handleClose} width='96px' />
+                            <BlackButton text='Store' width='96px' />
+                            <PurpleButton disabled={false} onClick={handleSend} heigth={'50px'} width='96px'>
+                                <div>Send</div>
+                            </PurpleButton>
+                        </div>
+                    </div>
+                    <Tooltip id="address-tooltip" />
+                </div>
+        }
+
+        {
+            transactionState.isLoading && 
+            <div style={{height:'450px', width:'365px', display:'flex', justifyContent:'space-between', flexDirection:'column', overflowX:'hidden' ,overflowY:'scroll'}}>
+                <div>
+
+                </div>
+                <div style={{marginBottom:'20px', marginTop:'30px',  background:'#222222', width:'365px'}}>
+                    <div style={{marginLeft:'20px', marginRight:'15px', display:'flex', justifyContent:'space-between'}}>
+                        <BlackButton text='Close' onClick={handleClose} width='320px' />
+                    </div>
                 </div>
             </div>
-            <div style={{marginBottom:'20px', marginTop:'30px',  background:'#222222', width:'365px'}}>
-                <div style={{marginLeft:'20px', marginRight:'15px', display:'flex', justifyContent:'space-between'}}>
-                    <BlackButton text='Close' onClick={handleClose} width='96px' />
-                    <BlackButton text='Store' width='96px' />
-                    <PurpleButton disabled={false} onClick={handleSend} heigth={'50px'} width='96px'>
-                        <div>Send</div>
-                    </PurpleButton>
+        }
+
+        {
+            transactionState.isFailure && 
+            <div style={{height:'450px', width:'365px', display:'flex', justifyContent:'space-between', flexDirection:'column', overflowX:'hidden' ,overflowY:'scroll'}}>
+                <div style={{height:'350px', display:'flex', flexDirection:'column',justifyContent:'center'}}>
+                    <div style={{display:'flex', justifyContent:'center'}}>
+                        <div style={{width:'80px', height:'80px', borderRadius:'50%', background:'#220000', display:'flex', justifyContent:'center'}}>
+                                <CloseIcon sx={{color:'#e5216f', marginTop:'25px',width:'30px', height:'30px'}} />
+                        </div>
+                    </div>
+                    <div style={{marginTop:'10px', color:'#fefdf9', fontSize:'30px',display:'flex', justifyContent:'center'}} className="headingHome">
+                        Failed!
+                    </div>
+                </div>
+                <div style={{marginBottom:'20px', marginTop:'30px',  background:'#222222', width:'365px'}}>
+                    <div style={{marginLeft:'20px', marginRight:'15px', display:'flex', justifyContent:'space-between'}}>
+                        <BlackButton text='Close' onClick={handleClose} width='320px' />
+                    </div>
                 </div>
             </div>
-            <Tooltip id="address-tooltip" />
-        </div>
+        }
+        {
+            transactionState.isSuccess && 
+            <div style={{height:'450px', width:'365px', display:'flex', justifyContent:'space-between', flexDirection:'column', overflowX:'hidden' ,overflowY:'scroll'}}>
+                <div style={{height:'350px', display:'flex', flexDirection:'column',justifyContent:'center'}}>
+                    <div style={{display:'flex', justifyContent:'center'}}>
+                        <div style={{width:'80px', height:'80px', borderRadius:'50%', background:'#22342b', display:'flex', justifyContent:'center'}}>
+                                <CheckIcon sx={{color:'#21e56f', marginTop:'25px',width:'30px', height:'30px'}} />
+                        </div>
+                    </div>
+                    <div style={{marginTop:'10px', color:'#fefdf9', fontSize:'30px',display:'flex', justifyContent:'center'}} className="headingHome">
+                        Sent!
+                    </div>
+                </div>
+                <div style={{marginBottom:'20px', marginTop:'30px',  background:'#222222', width:'365px'}}>
+                    <div style={{marginLeft:'20px', marginRight:'15px', display:'flex', justifyContent:'space-between'}}>
+                        <BlackButton text='Close' onClick={handleClose} width='320px' />
+                    </div>
+                </div>
+            </div>
+        }
+        </>
+
+
     )
 }
 
